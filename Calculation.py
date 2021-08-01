ed03f88d9ad815dc94b22be6e68472116c620b49
s4732222 <80511864+s4732222@users.noreply.github.com>
2021-08-02 00:27:10 +0800

    Add files via upload

---
+++ Calculation.py
@@ -0,0 +1,73 @@
+import math
+
+
+def td(T,RH):
+    r=(17.27*T/(237.7+T)+math.log(RH/100))
+    Td=237.7*r/(17.27-r)
+    return Td
+'''
+Td=td(10.9,64)
+print(Td)
+'''
+
+def Mechanical_mixing_height(T,Td,Pasquill,Uz,Z,Z0):
+    mechanical_mixing_height=(121/6)*(6-Pasquill)*(T-Td)+0.169*Pasquill*(Uz+0.257)/12*0.0000579*math.log(Z/Z0)
+    return mechanical_mixing_height
+
+'''
+print(Mechanical_mixing_height(10.9,Td,3,2.6,14,1.2))
+'''
+
+
+def Uv_value(uv,Uz):
+    grade=None
+    num=0
+    if uv>float(0.8) and Uz<float(2):
+        grade='等級A'
+
+    elif uv>float(0.8) and float(2)<=Uz<=float(5):
+        grade='等級B'
+    elif uv>float(0.8) and Uz>float(5):
+        grade='等級C'
+    elif float(0.4)<uv<=float(0.8) and Uz<float(3):
+        grade='等級B'
+    elif float(0.4)<uv<=float(0.8) and float(3)<=Uz<=float(5):
+        grade='等級C'
+    elif float(0.4)<uv<=float(0.8) and Uz>float(5):
+        grade='等級D'
+    elif float(0.1)<uv<=float(0.4)and Uz<float(2):
+        grade='等級B'
+    elif float(0.1)<uv<=float(0.4)and float(2)<=Uz<=float(5):
+        grade='等級C'
+    elif float(0.1)<uv<=float(0.4)and Uz>float(5):
+        grade='等級D'
+    elif uv<float(0.1)and Uz<float(2):
+        grade='等級F'
+    elif uv<float(0.1)and float(2)<=Uz<=float(3):
+        grade='等級E'
+    elif uv<float(0.1)and Uz>float(3):
+        grade='等級D'
+    else:
+        print('Error')
+    
+
+    if (grade=="等級A"):
+        num=1
+    if (grade=="等級B"):
+        num=2
+    if (grade=="等級C"):
+        num=3
+    if (grade=="等級D"):
+        num=4
+    if (grade=="等級E"):
+        num=5
+    if (grade=="等級F"):
+        num=6
+
+    return num
+
+
+
+
+
+
